6d01d83ac9bd94d312d3ac6893256abd60e5ae16
Muhaza <30515291+muhaza@users.noreply.github.com>
2020-01-23 18:09:40 +0800

    push all edit

--- io.js
+++ io.js
@@ -2,7 +2,7 @@ var app = new Vue({
     el: "#vue-card",
     data: {
         product2img: "./asset/image/teh-persada.webp",
-        product2Title: "Persada Teh Padu!",
+        product2Title: "Persada Teh",
         product2Desc: "Ubat Diabetes, Kanser, Anemia, Darah Tinggi, Membantu Detoks dan Menurunkan Berat Badan.",
         product2Url: "http://google.com",
         soonimg: "./asset/image/akan-datang.jpg",
