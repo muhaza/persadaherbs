f93229d5c5a196e9545c66be19e6d2dcebfdacfe
Muhaza <30515291+muhaza@users.noreply.github.com>
2020-01-28 15:29:56 +0800

    push

--- component/pill.js
+++ component/pill.js
@@ -22,10 +22,42 @@ document.getElementById("aboutPill").innerHTML = `
 <div class="tab-content" id="pills-tabContent">
     <div class="tab-pane fade show active" id="pills-profile" role="tabpanel" 
           aria-labelledby="pills-profile-tab">  
-        <div class="row">
-        <div class="col-sm-3">
-            <img class="img-fluid" src="/asset/image/ramuan-sihat.png" alt="">
+
+      <div class="row mb-3" style="background:url(./asset/image/header-bg.webp);padding:1rem 0;margin:1rem; border-radius:1rem;">
+        <div class="col-sm">
+          <div class="d-flex justify-content-center">
+            <img src="./asset/image/Faiziman.webp" style="box-shadow: 0px 5px 11px 0px #00000042;" class="rounded-circle" alt="Faiziman" width="200px"/> </span> 
+            <!-- <div class="card" style="width: 100%; margin-bottom:2rem;">
+              <img class="card-img-top" src="./asset/image/Faiziman.webp" alt="Card image cap">
+                <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div> -->
+          </div>
         </div>
+        <div class="col-sm">
+         <div class="d-flex justify-content-center">
+            <img src="./asset/image/Faiziwan.webp" style="box-shadow: 0px 5px 11px 0px #00000042;z-index: 10;" class="rounded-circle align-center" alt="Faiziwan" width="200px"/>
+            <!-- <div class="card" style="width: 100%; margin-bottom:2rem;">
+              <img class="card-img-top" src="./asset/image/Faiziwan.webp" alt="Card image cap">
+                <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div> -->
+         </div>
+        </div>
+        <div class="col-sm">
+          <div class="d-flex justify-content-center">
+            <img src="./asset/image/Faizli.webp" style="box-shadow: 0px 5px 11px 0px #00000042;z-index: 20;" class="rounded-circle align-center" alt="Faizli" width="200px"/>
+          </div>
+        </div>
+      </div>
+      
+
+        <div class="row">
+        <!-- <div class="col-sm-3">
+            <img class="img-fluid" src="/asset/image/ramuan-sihat.png" alt="">
+        </div> -->
         <div class="col-sm">
          <p> Persada herbs Sdn Bhd merupakan sebuah syarikat bumiputra diasaskan oleh 
         3 orang adik beradik dengan wawasan menunaikan perintah fardhu kifayah, iaitu 
