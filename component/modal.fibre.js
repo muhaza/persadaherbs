a8146394617d2801e68f2e7ffef575bc7aa1285d
Muhaza <30515291+muhaza@users.noreply.github.com>
2020-04-09 01:01:32 +0800

    commit faebr

--- component/modal.fibre.js
+++ component/modal.fibre.js
@@ -28,23 +28,239 @@ document.getElementById("modal-fibre").innerHTML = `
                                 <div style="width: 100%;">
                                     <img class="card-img-top" v-bind:src="fibreImg" alt=" 📚Card image cap" />
                                     <div class="card-body">
-                                        <h5 class="card-title"> 👍 KEBAIKAN</h5>
+
                                         <p class="card-text">
-                                        - Melegakan sembelit <br>
-                                        - Membersihkan usus besar dan usus kecil<br>
-                                        - Menghilangkan bau badan dan nafas berbau<br>
-                                        - Menstabilkan tekanan darah<br>
-                                        - Meningkatkan kecerahan kulit<br>
-                                        - Meningkatkan tahap kecergasan <br>
-                                        - Melancarkan proses penghadaman<br>
-                                        - Menyingkirkan toksin dari tubuh badan<br>
-                                        - Membantu mengatasi kulit yang bermasalah<br>
-                                        - Meningkatkan keanjalan dan keremajaan kulit<br>
-                                        - Mengawal paras gula dan kolestrol dalam darah<br>
-                                        - Membantu mengatasi masalah berat badan berlebihan<br>
-                                        - Meningkatkan tahap metabolisme bagi pembakaran lemak dan kalori<br>
-                                        - Memberi rasa kenyang sambil membantu mengawal selera makan<br>
-                                        - Mengatasi masalah perut buncit, membentuk tubuh yang lebih ramping dan langsing<br>
+                                        Di fahamkan secara umumnya 1 daripada 4 individu mengalami masalah ketidak lancar pengbuangan atau sembelit. Lebih serius lagi ketika umur mencapai 40 tahun keatas dengan kadar nisbahnya 1 daripada 3 individu.<br />
+<br />
+Pakar perubatan, Dr. John Harvey Kellog mendapati bahawa 90% daripada penyakit disebabkan kolon yang tidak berfungsi dengan baik. Sehubung dengan itu, Persatuan Perubatan British telah membuat kajian dan mendapati tidak kurang daripada 179 jenis penyakit yang berpunca daripada kegagalan kolon berfungsi dengan baik. Pada tahun 1980-an, Sylvester W Graham menjadi terkenal kerana kejayaannya memperkenalkan pengambilan makanan pelawas. untuk menjaga kesihatan usus.<br />
+<br />
+Oleh hal yang demikian pakar perubatan telah menjalankan penyelidikan yang serius terhadap peranan fibre dalam kesihatan.<br />
+<br />
+Tubuh kita bergantung sepenuhnya kepada zat-zat yang terkandung dalam makanan untuk melakukan segala aktiviti seharian. Kesihatan dan kebersihan usus menentukan keberkesanan penghadaman makanan, penyerapan zat-zat makanan dan proses-proses lain.<br />
+<br />
+<b>USUS YANG BERSIH ASAS KESIHATAN KITA</b><br />
+<b><br /></b>
+<br />
+<ul>
+<li>Adakah anda kerap kali terasa letih tanpa sebab yang nyata ?</li>
+<li>Adakah berat badan anda kian meningkat dan bonjolan lemak terkumpul,terutamanya di bahagian perut ?</li>
+<li>Adakah anda mengalami alergi kulit atau makanan ?</li>
+<li>Adakah anda kerap mengalami masalah sembelit, diarea, perut berangin, kembung dan kesenakan?</li>
+<li>Adakah anda sering dijangkiti selsema setiap beberapa minggu, sakit kepala, sakit belakang, nafas berbau,tubuh cepat berbau, mental yang keliru?</li>
+<li>Adakah kulit anda kelihatan pucat, pudar dan tidak bermaya ?</li>
+<li>Adakah anda tidur tidak nyenyak dan berasa penat tatkala bangun pagi ?</li>
+</ul>
+<br />
+<blockquote class="tr_bq">
+*SEKIRANYA ANDA MENJAWAB “YA” UNTUK MANA-MANA SOALAN DI ATAS, KEMUNGKINAN BESAR ANDA MERUPAKAN SEORANG DARIPADA BERJUTA MENGALAMI KOLON BERTOKSIK.</blockquote>
+<br />
+<b>MASALAH KESIHATAN LAIN YANG BERKAITAN RAPAT&nbsp;</b><b>DENGAN KOLON BERTOKSIK</b><br />
+<ol>
+<li>Arthritis-sakit sendi dan sengal-sengal&nbsp;otot</li>
+<li>Bau badan dan nafas berbau</li>
+<li>Kuku dan rambut rapuh</li>
+<li>Tekanan darah meningkat</li>
+<li>Kelemahan mental – daya ingatan dan&nbsp;tumpuan yang lemah</li>
+<li>Peredaran darah kurang baik – tangan&nbsp;dan kaki sejuk</li>
+<li>Kerap kali sembelit atau cirit-birit</li>
+<li>Mengidam makanan</li>
+<li>Kerap pening-pening lalat dan sakit kepala, kesakitan pada leher dan bahu</li>
+<li>Gejala putus haid, terasa putus haid, haid tidak menentu</li>
+<li>Mudah marah dan murung, rendah tenaga syahwat</li>
+<li>Masalah kulit seperti jerawat, akne, eczema,psoriasis</li>
+</ol>
+<br /><br /><b>APA DIA NAJIS KRONIK?</b><div>
+<b><br /></b><div>
+<div>
+Najis kronik adalah sejenis lapisan yang terbentuk akibat dari penimbunan najis pada dinding usus besar dalam masa yang bertahun tahun.</div>
+<div>
+<br /></div>
+<div>
+Tubuh kita bergantung sepenuhnya kepada zat-zat yang terkandung dalam</div>
+<div>
+makanan untuk melakukan segala aktiviti seharian. Kesihatan dan kebersihan usus menentukan keberkesanan penghadaman makanan, penyerapan zat-zat makanan dan proses-proses lain.</div>
+<div>
+<br /></div>
+<div>
+<div>
+Najis kronik yang termendap di dinding usus akan menghalang penyerapan</div>
+<div>
+zat-zat makanan dengan berkesan. Pereputan sisa makanan yang termendap</div>
+<div>
+tadi akan menghasilkan toksik yang akan dilepaskan kedalam sistem peredaran darah.</div>
+<div>
+<br /></div>
+<div>
+Selain itu juga, najis kronik boleh menjadi medan yang boleh membiakkan bacteria patogenik , yis, fungi, parasit, dan juga cacing atau protozoa yang boleh hasilkan lebih banyak toksin ke dalam sistem peredaran darah dan seterusnya akan di bawa ke seluruhan organ organ seperti buah pinggang, hati dalam tubuh badan kita. Kitaran yang berulangan ini merupakan punca kepada pelbagai penyakit.</div>
+<div>
+<br /></div>
+<div>
+<div>
+<b>ANTARA PUNCA KANSER</b></div>
+<div>
+<b>Kajian Dr Hulda Clark</b></div>
+</div>
+<div>
+<b><br /></b></div>
+<div>
+<b><br /></b></div>
+</div>
+<br />
+<b>Kanser</b><br />
+<b>↓</b><br />
+<b>Karsinogen</b><br />
+<b>↓</b><br />
+<b>Cacing/Bakteria</b><br />
+<b>↓</b><br />
+<b>Najis kronik</b><br />
+<b>↓</b><br />
+<b>Timbunan sisa makanan</b><br />
+<b>↓</b><br />
+<b>Sembelit / pembuangan</b><br />
+<b>↓</b><br />
+<br />
+<b>tidak lancar</b><br />
+<b><br /></b>
+<b><br /></b>
+<b><br /></b>
+<b>Gaut</b><br />
+<b>↓</b><br />
+<b>Kristal</b><br />
+<b>↓</b><br />
+<b>Purin (protien)</b><br />
+<b>↓</b><br />
+<b>Asid Urik</b><br />
+<b>↓</b><br />
+<b>Protein yang tidak dihadamkan</b><br />
+<b>↓</b><br />
+<br />
+<b>Masalah sistem penghadaman</b><br />
+<b><br /></b>
+<b><br /></b>
+<b>90% PENYAKIT BERPUNCA&nbsp;</b><b>DARIPADA USUS BESAR</b><br />
+<br />
+<b>Kajian Dr. John Harvey Kellog</b><br />
+<b><br /></b>
+<br />
+<ol>
+<li>GOUT</li>
+<li>KANSER JANTUNG</li>
+<li>KENCING MANIS</li>
+<li>PERUT BUNCIT KANSER USUS</li>
+<li>OSTEOARTHRITIS</li>
+</ol>
+<br />
+<br />
+<b>APAKAH KAITAN BARAH DENGAN NAJIS KRONIK?</b><br />
+<b><br /></b>
+Daripada kajian Dr Hulda clark. Belia mendapati fluks (fasiola hepatica) sejenis cacing dalam usus akan membiak dalam lapisan najis kronik. Semasa membiak, fluks akan menghasilkan sejenis bahan yang akan merangsang proses pembahagian sel yang tidak terkawal dan seterusnya menghasilkan barah.<br />
+<br />
+<b>BAHAN UTAMA DALAM FAÉBR</b><br />
+<b><br /></b>
+<b>EKSTRAK LIMAU</b><br />
+Mengandungi galian-galian dan vitamin terutama vitamin A.<br />
+Mengandungi asid sitrik yang boleh mengurangkan lemah badan<br />
+dan menguruskan badan, melegakan perut dan memudahkan<br />
+<br />
+penghadaman.<br />
+<br />
+<b>PSYLIUM HUSK</b><br />
+Fiber tumbuhan.<br />
+FDA-mengurangkan risiko penyakit jantung.<br />
+LDL-mengurangkan kolestrol tidak baik.<br />
+Membaiki tahap gula bagi pesakit diabetes.<br />
+<br />
+<b>EKSTRAK HALIA</b><br />
+Bertindak sebagai pemanas badan, salap, julap, pembuka selera<br />
+makan, dan juga penawar yang baik untuk memulihkan pelbagai<br />
+penyakit seperti batuk, selsema dan muntah-muntah, cirit -birit,<br />
+radang sendi, senak perut, mabuk laut, kolik, kolera, asma, sakit<br />
+kepala, dan juga aneroksia.<br />
+<br />
+<b>BUNGA ROSELLE</b><br />
+Membantu penyingkiran toksin dalam badan, membantu pergerakan<br />
+peristalsis usus, merendahkan kemungkinan sembelit.<br />
+Mengurangkan kepekatan darah, memperbaiki peredaran darah<br />
+dan merendahkan tekanan darah, mengandungi penyejuk<br />
+semula jadi, asid sitrus membantu menenangkan&amp;menurunkan suhu<br />
+<br />
+<b>OAT (BETA-GLUKAN)&nbsp;</b><br />
+<b>FRUCTOOLIGOSACCHARIDE</b><br />
+<b>HAWTHRON&nbsp;</b><br />
+<b>BERRY BLACKCURRANT</b><br />
+<br />
+<b>CARA FAÉBR BERTINDAK</b><br />
+<b><br /></b>
+Pengambilan Faébr<br />
+↓<br />
+Faébr menyerap air<br />
+&amp; berkembang<br />
+↓<br />
+Merangsang pergerakan<br />
+peristalsis usus<br />
+↓<br />
+Menyingkir koprostasis<br />
+↓<br />
+<br />
+Toksin disingkirkan<br />
+<br />
+<b>KEBAIKAN FAÉBR</b><br />
+<br />
+<ul>
+<li>Pelincir usus semula jadi mempertingkatkan&nbsp;pergerakan peristalisis, melegakan</li>
+<li>sembelit dan masalah akibat sembelit&nbsp;seperti buasir.</li>
+<li>Membantu menyingkirkan koprotasis,&nbsp;merendahkan risiko kanser kolon.</li>
+<li>Membersihkan kolon, mempertingkatkan&nbsp;persekitaran untuk pertumbuhan bektiria&nbsp;baik.</li>
+<li>Memberikan rasa kenyang, efektif untuk&nbsp;mengawal berat badan.</li>
+<li>Membantu mengawal paras lemak dan&nbsp;kolesterol dalam badan&nbsp;</li>
+<li>Membantu memulihkan semula sistem&nbsp;penghadaman</li>
+</ul>
+<br />
+<div>
+Adakah Colon Cleansing boleh membantu memulihkan penyakit kronik ?</div>
+<div>
+<div>
+Dari kajian ke atas ribuan penyakit, Dr. Bernand Jensen telah mendapati kesihatan</div>
+<div>
+kebanyakkan pesakit pulih secara beransur-ansur selepas menjalankan proses</div>
+<div>
+pembersihan kolon dan diikuti diet yang seimbang.</div>
+<div>
+<br /></div>
+<div>
+Adakah teknik Colon Cleansing member kesan sampingan dalam masa panjang ?</div>
+<div>
+Membersihkan kolon adalah perkara yang baik, namun kaedah yang digunakan</div>
+<div>
+mungkin mendatangkan mudarat seperti kecederaan pada usus dan kesakitan.</div>
+<div>
+Walaubagaimanapun, jika menggunakan kaedah yang betul, pembersihan kolon</div>
+<div>
+tidak mendatangkan apa-apa kemudaratan kepada anda.</div>
+<div>
+<br /></div>
+<div>
+Apakah kaedah yang terbaik untuk membersihkan kolon ?</div>
+<div>
+Kaedah yang terbaik adalah dengan menggunakan kaedah pemakanan yang</div>
+<div>
+dihasilkan dari buah-buahan dan sayur-sayuran semula jadi. Contohnya, Faébr.</div>
+<div>
+Berapa lamakah saya perlu mengambil Faébr untuk memberikan</div>
+<div>
+kesan yang optimum ?</div>
+<div>
+<br /></div>
+<div>
+Mengikut kajian daripada testimoni yang telah berjaya, ramai yang memperoleh</div>
+<div>
+kesannya dalam masa beberapa hari sa ja (5-7), namun yang sebaik-baiknya tidak</div>
+<div>
+kurang dari 15 hari.</div>
+</div>
+</div>
+</div>
+
                                                                                 </p>
                                         <div style="border-bottom: 1px #00000069 solid; margin-bottom: 10px;"></div>
                                         <h4 class="text-center">PENAWAR DAN ZAT TAMBAHAN</h4>
